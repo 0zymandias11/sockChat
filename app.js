563017b69d053d11628c75ef2b465faa3023b039
0zymandias11 <priyankarroy1711@gmail.com>
2024-06-21 21:17:13 +0530

    socket.io based chat application

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 let socketsConected = new Set()
 
-io.on('connection', onConnected)
-
-function onConnected(socket) {
+io.on('connection', (socket)=>{
   console.log('Socket connected', socket.id)
   socketsConected.add(socket.id)
   io.emit('clients-total', socketsConected.size)
@@ -31,4 +29,25 @@ function onConnected(socket) {
   socket.on('feedback', (data) => {
     socket.broadcast.emit('feedback', data)
   })
-}
+})
+
+// function onConnected(socket) {
+//   console.log('Socket connected', socket.id)
+//   socketsConected.add(socket.id)
+//   io.emit('clients-total', socketsConected.size)
+
+//   socket.on('disconnect', () => {
+//     console.log('Socket disconnected', socket.id)
+//     socketsConected.delete(socket.id)
+//     io.emit('clients-total', socketsConected.size)
+//   })
+
+//   socket.on('message', (data) => {
+//     // console.log(data)
+//     socket.broadcast.emit('chat-message', data)
+//   })
+
+//   socket.on('feedback', (data) => {
+//     socket.broadcast.emit('feedback', data)
+//   })
+// }
